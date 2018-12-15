5b038de873f23548d51f2c08f9e4e34c5a6c71ef
dengkaile <Dengkaile@outlook.com>
2018-12-15 19:08:07 +0800

    实体一对多查询，mysql中join连接语句的理解是实践

--- src/learnMybatis/TestMybatis.java
+++ src/learnMybatis/TestMybatis.java
@@ -15,23 +15,30 @@ public class TestMybatis {
 	public static void main(String[] args) throws IOException {
 		String resource = "mybatis-config.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);		
 		SqlSession session = sqlSessionFactory.openSession();
 		
-		//test User delete SQL
-		User user1 = new User();
-		user1.setId(2);
-		session.delete("deleteUser",user1);
-
+//		//test User delete SQL
+//		User user1 = new User();
+//		user1.setId(2);
+//		session.delete("deleteUser",user1);
+//
+//		//show all user
+//		List<User> users = session.selectList("listUser");
+//		for(User user : users){
+//			System.out.println(user.getName());
+//		}
 		
-		
-		
-		//show all user
+		//Ò»¶Ô¶à
 		List<User> users = session.selectList("listUser");
-		for(User user : users){
-			System.out.println(user.getName());
+		for (User user : users){
+			System.out.println(user);
+			List<Product> products = user.getProducts();
+			for (Product product : products ){
+				System.out.println("\t"+product);
+			}
 		}
+		
 		
 		session.commit();
 		session.close();
